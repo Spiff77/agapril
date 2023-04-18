9c38490272d337302982ba5f4b733b7f071293e3
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-04-18 12:11:03 +0300

    test

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ commitmessage=$1
 remotepath=git remote -v | grep push
 echo -e "git add current folder"
 git add .
-echo -e "commiting with message: " + $commitmessage
+echo -e "commiting with message: $commitmessage"
 git commit -m $commitmessage
-echo -e "pushing to :" + $remotepath
+echo -e "pushing to : $remotepath"
 git push origin main
